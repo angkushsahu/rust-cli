f10fa823b84551b11a94ff1b9d56c711d8f6b319
Angkush <angkysahu2502@gmail.com>
2024-02-28 21:13:03 +0530

    refactor: replaced loops with iterators

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,19 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Self, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
+    pub fn new(mut args: env::Args) -> Result<Self, &'static str> {
+        args.next(); // the first argument is not needed as it contains the path of the directory
 
-        let query = args[1].to_string();
-        let filename = args[2].to_string();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not receive a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not receive a file name"),
+        };
+
         let case_sensitive = env::var("CASE_SENSITIVE").is_err();
 
         Ok(Config { query, filename, case_sensitive })
@@ -21,24 +27,19 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
